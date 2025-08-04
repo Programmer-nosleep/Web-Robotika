c6efbfd72ad7a7dc8098ab884154ba1b7c663c2d
Programmer-nosleep <ahmadzanisy@gmail.com>
2025-08-04 09:03:08 +0700

    update

--- backend/helper/token.ts
+++ backend/helper/token.ts
@@ -1,17 +1,38 @@
-import type { Types } from "mongoose"
-import jwt from "jsonwebtoken"
+import jwt, { type JwtPayload, type VerifyCallback, sign, verify } from "jsonwebtoken";
+import { Types } from "mongoose";
 
-type GenerateTokenPayload = {
-  _id: Types.ObjectId
-}
+type TokenPayload = {
+  _id: Types.ObjectId;
+  [key: string]: any;
+};
 
-export const generateToken = ({ _id }: GenerateTokenPayload): string => {
-  const secret = process.env.SECRET_KEY
+export const generateAccessToken = (payload: TokenPayload): string => {
+  const secret = process.env?.ACCESS_TOKEN_SECRET;
+  if (!secret) throw new Error("ACCESS_TOKEN_SECRET is not defined");
 
-  if (!secret) 
-    throw new Error("token is not define.");
-    
-  return jwt.sign({ _id }, secret, {
-    expiresIn: '7d'
-  })
-}
+  return sign(payload, secret, { expiresIn: "30s" });
+};
+
+export const generateRefreshToken = (payload: TokenPayload): string => {
+  const secret = process.env?.REFRESH_TOKEN_SECRET;
+  if (!secret) throw new Error("REFRESH_TOKEN_SECRET is not defined");
+
+  return sign(payload, secret, { expiresIn: "7d" });
+};
+
+export const generateToken = (payload: TokenPayload): string => {
+  const secret = process.env?.SECRET_KEY;
+  if (!secret) throw new Error("SECRET_KEY is not defined");
+
+  return sign(payload, secret, { expiresIn: "7d" });
+};
+
+export const verifyToken = (
+  token: string,
+  secretKey: string = process.env?.REFRESH_TOKEN_SECRET!,
+  callback?: VerifyCallback<string | JwtPayload>
+) => {
+  if (!secretKey) throw new Error("Secret key is not provided for verification");
+
+  return verify(token, secretKey, callback);
+};
